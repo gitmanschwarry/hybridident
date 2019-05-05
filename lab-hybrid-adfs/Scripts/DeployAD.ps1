f9c4fb1da2c40d970452c6f0297b2642c0446bb4
schwarry <33959512+schwarry@users.noreply.github.com>
2019-05-05 16:25:07 -0400

    comment out code ADDeploy.ps1

--- lab-hybrid-adfs/Scripts/DeployAD.ps1
+++ lab-hybrid-adfs/Scripts/DeployAD.ps1
@@ -79,7 +79,8 @@ if (!(Test-Path -Path "$($completeFile)$step")) {
     $Gateway = $cfg.IPv4DefaultGateway.NextHop
     $MaskBits = $cfg.IPv4Address.PrefixLength
 
-    # Remove any existing IP, gateway from our ipv4 adapter
+    <#
+	# Remove any existing IP, gateway from our ipv4 adapter
     If (($adapter | Get-NetIPConfiguration).IPv4Address.IPAddress) {
         $adapter | Remove-NetIPAddress -AddressFamily $IPType -Confirm:$false
     }
@@ -87,7 +88,7 @@ if (!(Test-Path -Path "$($completeFile)$step")) {
     If (($adapter | Get-NetIPConfiguration).Ipv4DefaultGateway) {
         $adapter | Remove-NetRoute -AddressFamily $IPType -Confirm:$false
     }
-
+	#>
     #record that we got this far
     New-Item -ItemType file "$($completeFile)$step"
 }
